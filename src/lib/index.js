acf6ca048f31e70299c3d418dd0a99d3bd1006e6
Johnattas Santana <johnattas@gmail.com>
2025-06-18 14:39:47 -0300

    Default Component Vue3 - Named AdvancedChatAi

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,13 +1,1 @@
-import { defineCustomElement } from 'vue'
-import ChatWindow from './ChatWindow'
-
-export const AdvancedChatAi = ChatWindow
-export const VueAdvancedChat = defineCustomElement(ChatWindow)
-
-const PACKAGE_NAME = 'vue-advanced-chat'
-
-export function register() {
-	if (!customElements.get(PACKAGE_NAME)) {
-		customElements.define(PACKAGE_NAME, VueAdvancedChat)
-	}
-}
+export { ChatWindow } from './ChatWindow'
